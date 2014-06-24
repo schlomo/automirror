a8a186a1f887c6f63b16e9b562e5d473131109db
Schlomo Schapiro <github@schlomo.schapiro.org>
2014-06-24 13:56:08 +0200

    Initial push of script and tests

---
+++ automirror.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e -E -u
+
+XRANDR_STATUS_PROGRAM=${XRANDR_STATUS_PROGRAM:-xrandr}
+XRANDR_SET_PROGRAM=${XRANDR_SET_PROGRAM:-xrandr}
+
+PRIMARY_DISPLAY=${PRIMARY_DISPLAY:-LVDS1}
+
+# force called programs to english output
+LANG=C LC_ALL=C
+
+xrandr_current="$($XRANDR_STATUS_PROGRAM)"
+
+# find connected displays
+connected_displays=( $(sed -n -e 's/^\(.*\) connected.*mm$/\1/p' <<<"$xrandr_current") )
+display_list="$(sed -z -e 's/\n   / /g' <<<"$xrandr_current" | sed -n -e 's/^\([A-Z0-9]\+\) connected.* \([0-9]\+\)mm.* \([0-9]\+\)mm.* \([0-9]\+\)x\([0-9]\+\)[ 0-9\.\*]\++.*$/\1 \2 \3 \4 \5/p' )"
+: ${connected_displays[@]}
+: "$display_list"
+
+function die {
+    echo 1>&2 "$*"
+    exit 10
+}
+
+function get_display_resolution {
+    local find_display="$1" ; shift
+    local display_list="$1"
+    while read display width_mm height_mm width height  ; do
+        if [[ "$display" == "$find_display" ]] ; then
+            echo ${width}x${height}
+            return 0
+        fi
+    done <<<"$display_list"
+    die "Could not determine resolution for '$find_display'. Display Data:
+$display_list"
+}
+
+function get_highest_display {
+    local display_list="$1" ; shift
+    local data=( $(sort -r -n -k 5 <<<"$display_list") )
+    echo $data
+}
+
+
+# if primary display is active then that is our frame buffer size
+if [[ "${connected_displays[*]}" != *$PRIMARY_DISPLAY* ]] ; then
+    PRIMARY_DISPLAY=$(get_highest_display "$display_list")
+fi
+frame_buffer_resolution=$(get_display_resolution $PRIMARY_DISPLAY "$display_list")
+
+: $frame_buffer_resolution
+
+xrandr_set_args=( --fb $frame_buffer_resolution )
+if (( ${#connected_displays[@]} == 1 )) ; then
+    xrandr_set_args+=( --output $connected_displays --mode $frame_buffer_resolution --scale 1x1 )
+else
+    other_display_list="$(grep -v ^$PRIMARY_DISPLAY <<<"$display_list")"
+    $XRANDR_SET_PROGRAM $(while read display junk ; do echo " --output $display --scale 1x1 --off" ; done <<<"$other_display_list")
+    xrandr_set_args+=( --output $PRIMARY_DISPLAY --mode $frame_buffer_resolution --scale 1x1 )
+    while read display junk ; do
+        xrandr_set_args+=( --output $display --same-as $PRIMARY_DISPLAY --mode $(get_display_resolution $display "$other_display_list") --scale-from $frame_buffer_resolution  )
+    done <<<"$other_display_list"
+fi
+
+#logger -s -t "$0" -- Running $XRANDR_SET_PROGRAM "${xrandr_set_args[@]}"
+
+$XRANDR_SET_PROGRAM "${xrandr_set_args[@]}"
---
+++ runtests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+set -e -E -u
+
+function do_test {
+    local xrandr_output="$1" ; shift
+    local script_output="$1" ; shift
+    res="$( XRANDR_STATUS_PROGRAM="cat $xrandr_output" XRANDR_SET_PROGRAM=echo ./automirror.sh)"
+    if [[ "$res" == "$(< $script_output)" ]] ; then
+        return 0
+    else
+        echo "Difference:"
+        diff -u --label EXPECTED_RESULT --label ACTUAL_RESULT -- $script_output - <<<"$res"
+        return 1
+    fi
+}
+
+failed=0
+
+for test_case in testdata/*.txt ; do
+    [[ "$test_case" == *result.txt ]] && continue
+    script_output=${test_case%.txt}_result.txt
+    if [[ "$script_output" && -r "$script_output" ]] ; then
+        if do_test "$test_case" "$script_output" ; then
+           echo "OK $test_case" 
+        else
+            let failed++ 1
+            echo "FAILED $test_case"
+        fi
+    else
+        echo "Missing test result '$script_output'"
+    fi
+done
+
+if (( failed == 0 )) ; then
+    exit 0
+else
+    echo $failed TESTS FAILED
+    exit 1
+fi
